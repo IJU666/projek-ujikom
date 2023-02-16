a70bf9823dd9226af2ea150f4bcb408a5ed856e2
IJU666 <fauzimaulanabandung2020@gmail.com>
2023-02-16 11:07:44 +0700

    a

---
+++ database/migrations/2023_02_16_032848_create_pengaduan_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengaduanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengaduans', function (Blueprint $table) {
+            $table->id();
+            $table->string('isi');
+            $table->string('alamat');
+            $table->date('tglpengaduan');
+            $table->string('lampiran')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengaduans');
+    }
+}
