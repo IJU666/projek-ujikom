0e26db7c83d9ee3d85a58a4b454f6188df7c06e7
IJU666 <fauzimaulanabandung2020@gmail.com>
2023-02-16 09:28:53 +0700

    bisa

--- resources/views/data/sidebar.blade.php
+++ resources/views/data/sidebar.blade.php
@@ -4,7 +4,7 @@
         <div class="sidebar-header position-relative">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="logo">
-                    <a href="" class="text-decoration-none text-secondary">ASPERU</a>
+                    <a href="" class="text-decoration-none text-secondary">ASPERA</a>
                 </div>
                 <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
                     <div class="form-check fs-6">
